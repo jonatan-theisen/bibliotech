477afd22dde5a50d75c8ef94c3280d6b994c06c4
jonatan-theisen <jonatan.a.theisen@gmail.com>
2023-05-10 00:04:01 -0300

    Conectando com o banco de dados

--- src/bibliotech/Bibliotech.java
+++ src/bibliotech/Bibliotech.java
@@ -4,17 +4,61 @@
  */
 package bibliotech;
 
+import java.sql.*;
+import javax.swing.JOptionPane;
+import tela.FrmPrincipal;
+
 /**
  *
  * @author jonatan
  */
 public class Bibliotech {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static Connection conexao = null;
+    
+    public static void main(String[] args) 
+    {
+        if (abrirConexao())
+        {
+        new FrmPrincipal().setVisible(true); 
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar no Banco de Dados!");
+        }
+    }
+    
+    private static boolean abrirConexao() 
+    {
+        try
+        {
+            String dbdriver = "org.postgresql.Driver";
+            String dburl = "jdbc:postgresql://localhost:5432/bibliotech";
+            String dbuser = "postgres";
+            String dbpassword = "postgres";
+            
+          
+            // Carrega Driver do Banco de Dados
+            Class.forName(dbdriver);
+            
+            if (dbuser.length() != 0) // conexão com usuário e senha
+            {
+                conexao = DriverManager.getConnection(dburl, dbuser, dbpassword);
+            }
+            else
+            {
+                conexao = DriverManager.getConnection(dburl);
+            }
+            
+            return true;
+            
+        }
+        catch (Exception e)
+        {
+            System.err.println("Erro ao tentar conectar: " + e);
+            
+            return false;
+        }
     }
     
 }
